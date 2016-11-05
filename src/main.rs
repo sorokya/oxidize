fa81ca150ee0b3cea544d50b8326829160e4b961
Nick Moore <nick_r_moore@hotmail.com>
2016-11-05 00:06:48 -0400

    Load ROM from file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+use std::fs;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let rom_name = env::args().nth(1).unwrap();
+    
+    let mut rom = fs::File::open(&rom_name).unwrap();
+    let mut rom_buffer = Vec::new();
+    rom.read_to_end(&mut rom_buffer).unwrap();
+    
+    let rom_buffer = rom_buffer;
 }
